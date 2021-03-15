a19c6c1feb43e89182970c9c0a43bd91a227f017
mehadi-500 <mehadi.cosmo@gmail.com>
2021-03-15 17:56:36 +0600

    recape-auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
+  const [user, setUser] = useState({});
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    // [START auth_google_signin_popup]
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user, token);
+        setUser(user);
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential)
+      });
+    // [END auth_google_signin_popup]
+
+  }
+
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        console.log('FB user', user, accessToken);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode,errorMessage, email, credential)
+      });
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <button onClick={handleGoogleSignIn}>Sign In using Google</button>
+      <br />
+      <button onClick={handleFacebookSignIn}>Sign In using Facebook</button>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
+
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmjKyUN-Y8K0ZFdHN9SXmn072TFe495vo",
+    authDomain: "reacap-auth.firebaseapp.com",
+    projectId: "reacap-auth",
+    storageBucket: "reacap-auth.appspot.com",
+    messagingSenderId: "973816254330",
+    appId: "1:973816254330:web:1554f83ae2d2284191d0c3"
+  };
+  export default firebaseConfig;
